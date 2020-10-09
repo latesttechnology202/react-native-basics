3a63f6f6deb3b6c46b5e078fd8beb92a161a55d3
latest technology <72259677+latesttechnology202@users.noreply.github.com>
2020-10-09 14:52:55 +0530

    Create flexPropety.js
    add flex property file

---
+++ flexPropety.js
@@ -0,0 +1,38 @@
+import React from "react";
+import {View,Text,StyleSheet } from "react-native";
+//hello
+//in this tutorial we will learn about how
+//to use flex property in react native
+const App = () => {
+  return (
+    <View style={styles.container}>
+      <View style={styles.box1}>
+      </View>
+      <View style={styles.box2}>
+      </View>
+      <View style={styles.box3}>
+      </View>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container:{
+    backgroundColor:'pink',
+    flex:1
+  },
+  box1:{
+    backgroundColor:'powderblue',
+    flex:1
+  },
+  box2:{
+    backgroundColor:'grey',
+    flex:2
+  },
+  box3:{
+    backgroundColor:'orange',
+    flex:3
+  }
+});
+
+export default App;
